4a4538372a7cc98cb60896d26b56f511513e4a32
fukaiyas <yasuyuki.fukai@gmail.com>
2014-12-11 01:03:37 +0900

    タッチ操作のお試し

--- src/com/bugworm/ac2014/SyobochimController.scala
+++ src/com/bugworm/ac2014/SyobochimController.scala
@@ -1,10 +1,27 @@
 package com.bugworm.ac2014
 
-import javafx.fxml.FXML
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.fxml.{Initializable, FXML}
 import javafx.scene.control.Label
+import javafx.scene.input.TouchEvent
+import javafx.scene.layout.Pane
 
-class SyobochimController {
+import scalafx.animation.{Timeline, KeyFrame}
+
+object SyobochimController{
+  var instance : SyobochimController = _
+}
+class SyobochimController extends Initializable{
 
   @FXML
   var score : Label = _
+
+  @FXML
+  var mainScreen : Pane = _
+
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    SyobochimController.instance = this
+    new Loop().play();
+  }
 }
